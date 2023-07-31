13c31796c43c719df3938dab464d3bf71bd83c2e
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-01 00:47:19 +0300

    styling of Job card with tailwind

--- src/Components/Job.js
+++ src/Components/Job.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { dataContext } from '../data/DataContextProvider'
 
 function Job() {
+  const value = useContext(dataContext)
+  console.log(value)
+
   return (
-    <div>
-     <h2 className='text-3xl font-bold underline'>hello Job</h2> 
+    <div className="flex justify-center">
+      <div className="border p-6 max-w-2xl rounded-lg space-y-5">
+          <div>
+              <h4 className="hover:underline font-bold text-xl">Picker / Packer</h4>
+              <p className="text-lg">NoGigiddy</p>
+              <p className="text-lg">Houston, TX</p>
+          </div>
+
+          <div>
+              <span className="bg-gray-300 mr-3 p-2 rounded-md">$1900</span>
+              <span className="bg-gray-300 mr-3 p-2 rounded-md">Full-Time</span>
+          </div>
+
+          <div>
+              <p className="font-light mt-8">Acts Conscientiously: Demonstrates concern for quality, product integrity, and safety of one’s own and others’ work and impact to the stores.</p>
+          </div>
+      </div>
     </div>
   )
 }
