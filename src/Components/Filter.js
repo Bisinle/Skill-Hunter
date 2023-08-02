d3bacc0cfa8de945f36f2e0b4cfabfe6a9311644
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-02 15:13:08 +0300

    refactor Filter component and style search and filter fields

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -23,30 +23,44 @@ function Filter({ onFilter, onLocationFilter }) {
     }
   }
 
+  //making the location for filtering dynamic
+  const locations = [
+    "Nairobi",
+    "Mombasa",
+    "Kisumu",
+    "Nakuru",
+    "Eldoret",
+    "Machakos",
+    "Kisii",
+    "Nyeri",
+    "Embu",
+    "Kakamega",
+  ];
+
   return (
     <>
       {/* Filter dropdown's JSX */}
       <div className="container d-flex felx-row justify-content-center align-items-center">
         <div className="d-flex filter">
-          <h4>Filter by location: </h4>
+          <h4>Filter: </h4>
           <div>
             {/*onChange event to get selected location
                value="" ensures that there is no selected option by default
             */}
-            <select onChange={handleFilter}>
-              <option value=""></option>
-              <option value="nairobi">Nairobi</option>
-              <option value="mombasa">Mombasa</option>
-              <option value="kisumu">Kisumu</option>
-              <option value="nakuru">Nakuru</option>
-              <option value="eldoret">Eldoret</option>
-              <option value="machakos">Machakos</option>
-              <option value="kisii">Kisii</option>
-              <option value="nyeri">Nyeri</option>
-              <option value="embu">Embu</option>
-              <option value="kakamega">Kakamega</option>
+            <select
+              onChange={handleFilter}
+              className="text-sm text-gray-800 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+            >
+              <option value="">Filter by location:</option>
+              {locations.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
             </select>
+
           </div>
+          
         </div>
       </div>
     </>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -84,30 +84,35 @@ function Home() {
     );
   });
 
-  console.log(displayCareerdata);
+
   return (
     <>
-    <Filter onFilter={handleFilterChange} onLocationFilter={handleLocationFilter}/>
+    {/* <Filter onFilter={handleFilterChange} onLocationFilter={handleLocationFilter}/> */}
       <div>
         <div className="grid grid-cols-2 justify-center">
           <div>
-            <section class="py-px lg:pb-18 mb-1 bg-gray-100 overflow-hidden">
-              <div class="container px-4 mx-auto">
+            <section className="py-px lg:pb-18 mb-1 bg-gray-100 overflow-hidden">
+              <div className="container px-4 mx-auto">
 
-              {/* Form input for getting the search term */}
-              <form>   
-                 <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                 <div class="relative">
-                    <input type="search" id="default-search" value={searchTerm} onChange={handleSearch} class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Job Title"/>
+                {/* Search bar and filter dropdown */}
+                <div className="relative flex max-w-3xl mb-5 mt-5 mx-auto">
+                  <input
+                    type="text"
+                    id="default-search"
+                    value={searchTerm}
+                    onChange={handleSearch}
+                    placeholder="Search"
+                    className="w-full px-4 py-2.5 pl-10 text-sm text-gray-800 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  />
+                  <Filter onFilter={handleFilterChange} onLocationFilter={handleLocationFilter} />
                 </div>
-              </form>
-              
-              {/* Render the filtered careers */}
-              {displayCareerdata.length === 0 ? (
-                <p className="text-red-600">No Result Found</p>
-                ) : (
-                  displayCareerdata)}
 
+                {/* Render the filtered careers */}
+                {displayCareerdata.length === 0 ? (
+                  <p className="text-red-600">No Result Found</p>
+                  ) : (
+                    displayCareerdata
+                )}
 
               </div>
             </section>
@@ -118,6 +123,7 @@ function Home() {
           </div>
         </div>
       </div>
+
     </>
   );
 }
