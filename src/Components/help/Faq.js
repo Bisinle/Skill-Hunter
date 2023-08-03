69a1e8ecea2f10709b86f87b22d8633a15130198
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-03 09:29:08 +0300

    fetched the faq and displayed using map

--- src/Components/help/Faq.js
+++ src/Components/help/Faq.js
@@ -1,48 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { MdSwitchAccount  } from "react-icons/md"
+import { dataContext } from '../../data/DataContextProvider'
 
 function Faq() {
+  const { faqData } = useContext(dataContext)
+
+  const displayFaqData = faqData.map(({ title, questions}) => {
+    const faqQuestions = questions.map((text) => {
+      return (<Link to="#" className="block pb-1 underline text-sky-800">{text}</Link>)
+    })
+
+    return (
+        <div className="question border p-3 rounded-lg border-gray-500">
+          <MdSwitchAccount size={"3rem"} className="text-pink-700" />
+          <p className="my-2 text-2xl font-medium hover:underline hover:cursor-pointer"><strong>{title}</strong></p>
+            {faqQuestions}
+        </div>
+    )
+  })
+
   return (
     <div className="faq">
     <h3 className="text-2xl text-center font-bold mb-4">Frequently Asked Question</h3>
 
     <div className="grid grid-cols-2 gap-4">
-      <div className="question border p-3 rounded-lg border-gray-500">
-        <MdSwitchAccount size={"3rem"} className="text-pink-700" />
-        <p className="my-2 text-2xl font-medium hover:underline hover:cursor-pointer"><strong>My Account</strong></p>
-        <Link to="#" className="block pb-1 underline text-sky-800">How do I close account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">How do I reset my account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">I can't Sign in to My account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">How Do I Create an Indeed Account?</Link>
-      </div>
-
-      <div className="question border p-3 rounded-lg border-gray-500">
-        <MdSwitchAccount size={"3rem"} className="text-pink-700" />
-        <p className="my-2 text-2xl font-medium hover:underline hover:cursor-pointer"><strong>Job Alerts</strong></p>
-        <Link to="#" className="block pb-1 underline text-sky-800">How do I close account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">How do I reset my account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">I can't Sign in to My account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">How Do I Create an Indeed Account?</Link>
-      </div>
-
-      <div className="question border p-3 rounded-lg border-gray-500">
-        <MdSwitchAccount size={"3rem"} className="text-pink-700" />
-        <p className="my-2 text-2xl font-medium hover:underline hover:cursor-pointer"><strong>Job Search </strong></p>
-        <Link to="#" className="block pb-1 underline text-sky-800">How do I close account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">How do I reset my account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">I can't Sign in to My account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">How Do I Create an Indeed Account?</Link>
-      </div>
-
-      <div className="question border p-3 rounded-lg border-gray-500">
-        <MdSwitchAccount size={"3rem"} className="text-pink-700" />
-        <p className="my-2 text-2xl font-medium hover:underline hover:cursor-pointer"><strong>My Account</strong></p>
-        <Link to="#" className="block pb-1 underline text-sky-800">How do I close account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">How do I reset my account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">I can't Sign in to My account</Link>
-        <Link to="#" className="block pb-1 underline text-sky-800">How Do I Create an Indeed Account?</Link>
-      </div>
+      {displayFaqData}
     </div>
   
   </div>
--- src/data/DataContextProvider.js
+++ src/data/DataContextProvider.js
@@ -4,16 +4,22 @@ import { useContext, useState, useEffect } from "react";
 const dataContext = createContext();
 function DataContextProvider({ children }) {
   const [careerData, setCareerData] = useState([]);
+  const [faqData, setFaqData] = useState([])
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`https://skill-hunter-server.onrender.com/careers`)
+    fetch(`http://localhost:3000/careers`)
       .then((res) => res.json())
       .then((data) => setCareerData(data))
       .finally(setIsLoading(false));
+
+    fetch(`http://localhost:3000/faq`)
+      .then((res) => res.json())
+      .then((data) => setFaqData(data))
+      .finally(setIsLoading(false));
   }, []);
 
-  const values = { careerData, setCareerData, setIsLoading };
+  const values = { careerData, setCareerData, setIsLoading, faqData, setFaqData };
   return <dataContext.Provider value={values}>{children}</dataContext.Provider>;
 }
 
