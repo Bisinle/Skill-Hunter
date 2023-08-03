9bb83ef38287c4baca4f97440f1cd68509345041
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-03 08:05:19 +0300

    styling a question card

--- src/Components/help/Faq.js
+++ src/Components/help/Faq.js
@@ -1,65 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Faq() {
   return (
     <div className="faq">
-    <h3>Frequently Asked Question</h3>
-    <div className="question">
-      <p>
-        <strong>Lorem ipsum dolor, sit amet</strong>
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quod.
-        Impedit, laboriosam. Eos quibusdam voluptas deserunt aspernatur, dolor
-        odio cum, hic fugit reprehenderit excepturi totam perspiciatis officia
-        error, modi ipsum!
-      </p>
-    </div>
-    <div className="question">
-      <p>
-        <strong>Lorem ipsum dolor, sit amet</strong>
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quod.
-        Impedit, laboriosam. Eos quibusdam voluptas deserunt aspernatur, dolor
-        odio cum, hic fugit reprehenderit excepturi totam perspiciatis officia
-        error, modi ipsum!
-      </p>
-    </div>
-    <div className="question">
-      <p>
-        <strong>Lorem ipsum dolor, sit amet</strong>
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quod.
-        Impedit, laboriosam. Eos quibusdam voluptas deserunt aspernatur, dolor
-        odio cum, hic fugit reprehenderit excepturi totam perspiciatis officia
-        error, modi ipsum!
-      </p>
-    </div>
-    <div className="question">
-      <p>
-        <strong>Lorem ipsum dolor, sit amet</strong>
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quod.
-        Impedit, laboriosam. Eos quibusdam voluptas deserunt aspernatur, dolor
-        odio cum, hic fugit reprehenderit excepturi totam perspiciatis officia
-        error, modi ipsum!
-      </p>
-    </div>
-    <div className="question">
-      <p>
-        <strong>Lorem ipsum dolor, sit amet</strong>
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quod.
-        Impedit, laboriosam. Eos quibusdam voluptas deserunt aspernatur, dolor
-        odio cum, hic fugit reprehenderit excepturi totam perspiciatis officia
-        error, modi ipsum!
-      </p>
-    </div>
+    <h3 className="text-2xl text-center font-bold mb-4">Frequently Asked Question</h3>
 
+    <div className="grid grid-cols-2 gap-4">
+      <div className="question border p-3">"block pb-1
+"block pb-1
+"block pb-1
+"block pb-1
+        <p className="mb-2 text-2xl font-medium"><strong>My Account</strong></p>
+        <Link to="#" className="block pb-1 underline text-sky-800">How do I close account</Link>
+        <Link to="#" className="block pb-1 underline text-sky-800">How do I reset my account</Link>
+        <Link to="#" className="block pb-1 underline text-sky-800">I can't Sign in to My account</Link>
+        <Link to="#" className="block pb-1 underline text-sky-800">How Do I Create an Indeed Account?</Link>
+      </div>
+
+      <div className="question">
+        <p>
+          <strong>My Account</strong>
+        </p>
+        <a href="#">How do I close account</a>
+        <a href="#">How do I reset my account</a>
+        <a href="#">I can't Sign in to My account</a>
+        <a href="#">How Do I Create an Indeed Account?</a>
+      </div>
+
+      <div className="question">
+        <p>
+          <strong>My Account</strong>
+        </p>
+        <a href="#">How do I close account</a>
+        <a href="#">How do I reset my account</a>
+        <a href="#">I can't Sign in to My account</a>
+        <a href="#">How Do I Create an Indeed Account?</a>
+      </div>
+    </div>
   
   </div>
   )
