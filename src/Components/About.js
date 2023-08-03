04b9381d7dcfb00eaf55a693ae034d270c11aefd
Your Name <youremail@example.com>
2023-08-03 18:19:53 +0300

    ft-footer

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,27 +1,20 @@
-// import React from "react";
-
-// function About() {
-//   return (
-//     <div className="about-us">
-//       <h1>
-//         About Us
-//       </h1>
-//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam similique autem non eius magni. Mollitia non qui, excepturi laborum nulla omnis voluptate labore facere magni error est minus assumenda repudiandae! Similique magnam maxime itaque nesciunt sunt sapiente sint doloremque iste minima, cupiditate enim necessitatibus nemo aperiam pariatur error nihil natus.
-    
-//       </p>
-//     </div>
-//   )
-
-// }
-
-// export default About;
 // src/components/About.js
 import React, { useState } from "react";
 import "./About.css";
 
+import missionImage from "./Images/img.png";
+import visionImage from "./Images/img2.png";
+import achievementsImage from "./Images/img4.png";
+
 const About = () => {
   const tabs = ["Mission", "Vision", "Achievements"];
   const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const tabImages = {
+    Mission: missionImage,
+    Vision: visionImage,
+    Achievements: achievementsImage,
+  };
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
@@ -31,12 +24,11 @@ const About = () => {
     <div className="about">
       <div className="row">
         <div className="column">
-        <img
+          <img
             className="about-img"
-            src={require("./Images/img.png") }
-            alt=""
+            src={tabImages[activeTab]}
+            alt={activeTab}
           />
-          {/* <div className="about-img"></div> */}
         </div>
 
         <div className="column">
@@ -56,34 +48,30 @@ const About = () => {
             {/* About Content */}
             {activeTab === "Mission" && (
               <div className="content">
-                <h2>Mission</h2>
                 <p>
                 Skill Hunter empowers careers globally, connecting talent with opportunities. We revolutionize job searches through innovation and personalized support, fostering diversity and professional growth. Our mission is to celebrate individual talents, enabling everyone to unlock their full potential and find success in their dream careers. Together, we shape a future where possibilities are limitless.
                 </p>
-            
               </div>
             )}
 
             {/* Skills Content */}
             {activeTab === "Vision" && (
               <div className="content">
-                <h2>Vision</h2>
                 <p>
-                Empowering global careers. Skill Hunter envisions a future where talents are recognized, opportunities abound, and individuals thrive in fulfilling and meaningful careers.
-
-                </p>
+                  Empowering global careers. Skill Hunter envisions a future where talents are recognized, opportunities abound, and individuals thrive in fulfilling and meaningful careers.
+                  Skill Hunter aims to be the premier destination for talent discovery, driving positive change in the global workforce and creating a world where everyone finds purpose and fulfillment in their dream career.
+                  </p>
               </div>
             )}
 
             {/* Experiences Content */}
             {activeTab === "Achievements" && (
               <div className="content">
-                <h2>Achievements</h2>
                 <p>
-                <p>1. Skill Hunter has connected thousands of job seekers with their dream opportunities across various industries.</p>
-               <p> 2. Our innovative platform has received accolades for its user-friendly interface and personalized job search experience.</p>
-               <p> 3. Skill Hunter's commitment to diversity and inclusion has resulted in a diverse and vibrant community of talented individuals.</p>
-               <p> 4. We take pride in empowering countless individuals to unlock their potential and advance their careers on a global scale. </p>
+                  <p>1. Skill Hunter has connected thousands of job seekers with their dream opportunities across various industries.</p>
+                  <p>2. Our innovative platform has received accolades for its user-friendly interface and personalized job search experience.</p>
+                  <p>3. Skill Hunter's commitment to diversity and inclusion has resulted in a diverse and vibrant community of talented individuals.</p>
+                  <p>4. We take pride in empowering countless individuals to unlock their potential and advance their careers on a global scale.</p>
                 </p>
               </div>
             )}
@@ -95,4 +83,3 @@ const About = () => {
 };
 
 export default About;
-
