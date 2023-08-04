57736c1ffeae1351e337edc3d19959607183e080
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-04 03:21:35 +0300

    changed the applicants link

--- src/data/DataContextProvider.js
+++ src/data/DataContextProvider.js
@@ -22,7 +22,7 @@ function DataContextProvider({ children }) {
       .then((data) => setFaqData(data))
       .finally(setIsLoading(false));
 
-    fetch(` http://localhost:4000/Applicants`)
+    fetch(`https://skill-hunter-server.onrender.com/Applicants`)
       .then((res) => res.json())
       .then((data) => setApplicantsData(data));
   }, [newlyPosted, isDeleted]);
